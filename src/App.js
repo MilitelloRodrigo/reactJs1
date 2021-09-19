b6f9656acdf89d5e34d97656ad5e3136788ddd29
Rodrigo Militello <rodrigomilitello@MacBook-Air-de-Rodrigo.local>
2021-09-19 20:19:27 -0300

    cambio de nombres de lista mock

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import Exprenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 const expensesMock = [
-  { id: "e1", title: "Toile paper", amount: 4.28, date: new Date(2021, 2, 1) },
-  { id: "e2", title: "New Tv", amount: 124.48, date: new Date(2021, 2, 12) },
+  {
+    id: "e1",
+    title: "Papel Higienico",
+    amount: 4.28,
+    date: new Date(2021, 2, 1),
+  },
+  {
+    id: "e2",
+    title: "Nueva Television",
+    amount: 124.48,
+    date: new Date(2021, 2, 12),
+  },
   {
     id: "e3",
-    title: "New desktop",
+    title: "nuevo escritorio",
     amount: 139.18,
     date: new Date(2021, 2, 11),
   },
   {
     id: "e4",
-    title: "Car insurance",
+    title: "Seguro del auto",
     amount: 294.68,
     date: new Date(2021, 2, 21),
   },
